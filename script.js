01f043154fb2a3d7d7b6202c85ff7b739e30b452
msyed2020 <mikailsyed@yahoo.com>
2025-01-17 17:45:34 -0500

    Messing around lol

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ var inp = document.getElementById("numInp");
 
 function submit() {
 
-  document.addEventListener("DOMContentLoaded", () => {
+  
       
       var accept = document.getElementById("accept");
     
-      accept.addEventListener("click", () => {
-        var inpVal = parseFloat(inp.value);
+      var inpVal = parseFloat(inp.value);
     
-        if (!isNaN(inpVal)) {
-          console.log("The entered number is:", inpVal);
-        } else {
-          console.error("Please enter a valid number.");
-        }
-      });
-    });
+      if (!isNaN(inpVal)) {
+        console.log("The entered number is:", inpVal);
+      } else {
+        console.error("Please enter a valid number.");
+      }
+     
+    
 
 }
 
 function clear() {
-  inp.value = 0;
-  var cleared = document.getElementById("clear");
-  cleared.addEventListener("click", () => {
+  var inp = document.getElementById("numInp");
+    inp.value = 0;
+    var cleared = document.getElementById("clear");
     var inpVal = parseFloat(inp.value);
     console.log("Cleared, your value is " + inpVal);
-  })
+
+  
 }
 
 function add() {
